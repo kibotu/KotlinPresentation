827fe12f05a49b76332cb063219945cbad822591
Jan Rabe <jan.rabe@exozet.com>
2016-11-14 19:56:14 +0100

    - default gradle tasks: - Standard Libraries - default folder structure - Application, Activity, Dialog defaults - kotlin support - permission entry points, e.g.: location permission - unlocking screen on app start during debug - logging build and device info on app start - easy shared preferences handling - connectivity change events - handling of passed intent data - timebombing old versions - update and force update checks against latest google play - debug menu for readme, changelog, app start, restart, build info, runtime language change - backstack handling for fragments and debug drawer menu - enabled multidex - handling configuration changes - roboelectric tests

---
+++ app/src/main/java/net/kibotu/base/ui/BaseDialogFragment.java
@@ -0,0 +1,87 @@
+package net.kibotu.base.ui;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.common.android.utils.interfaces.LayoutProvider;
+import com.common.android.utils.interfaces.LogTag;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.KITKAT;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.LOLLIPOP_MR1;
+import static com.common.android.utils.ContextHelper.getFragmentActivity;
+
+/**
+ * Created by jan.rabe on 26/08/16.
+ * <img src="https://raw.githubusercontent.com/Aracem/android-lifecycle/master/complete_android_fragment_lifecycle.png"/>
+ */
+
+public abstract class BaseDialogFragment extends DialogFragment implements LayoutProvider, LogTag {
+
+    private Unbinder unbinder;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Dialog dialog = super.onCreateDialog(savedInstanceState);
+        final Window window = dialog.getWindow();
+        addFlags(window);
+        return dialog;
+    }
+
+    private static void addFlags(@Nullable Window window) {
+        if (window == null)
+            return;
+
+        if (SDK_INT >= LOLLIPOP)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        if (SDK_INT >= KITKAT)
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+        window.setBackgroundDrawableResource(android.R.color.transparent);
+
+        if (SDK_INT >= LOLLIPOP_MR1) {
+            window.setClipToOutline(false);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayout(), container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @NonNull
+    public String tag() {
+        return getClass().getSimpleName();
+    }
+
+    public BaseDialogFragment show() {
+        if (getFragmentActivity().getSupportFragmentManager().findFragmentByTag(getClass().getCanonicalName()) != null)
+            return this;
+
+        show(getFragmentActivity().getSupportFragmentManager(), getClass().getCanonicalName());
+        return this;
+    }
+}
